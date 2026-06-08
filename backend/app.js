fde6fb3bc68b2868deef43374a2f35fe0418fa22
Omar Williams <willio4@unlv.nevada.edu>
2026-06-08 11:52:27 -0700

    fix: improve CORS error message for better clarity

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,22 @@
 import express from "express";
-import endpointRouter from './src/routes/endpoints_router.js'
+import endpointRouter from './src/routes/endpoints_router.js';
 import cors from 'cors';
 
 const app = express();
 
 const allowedOrigins = ['http://localhost:3000', process.env.FRONTEND_URL];
+
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error('Blocked by CORS platform security policy.'));
     }
-  }
+  },
+  credentials: true
 }));
 
-app.use(cors({ origin: 'http://localhost:3001' }));
 app.use(express.json());
 app.use(endpointRouter);
 
